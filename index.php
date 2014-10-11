820c103d181adcb8199b462bce565826b50267cb
F. Javier R. Donado <fjrd84@gmail.com>
2014-10-11 22:05:19 +0200

    Color assignation for notes - CSS classes.

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
-<?php 
-for($i = 0; $i < 5; $i++){
-echo 'testeando!!';
-}
+<!DOCTYPE head PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/general.css">
+</head>
+<body>
+<div class="notes">
+<div class="note">Notes</div>
+<div class="note cnote">C</div>
+<div class="note csharpnote">C#</div>
+<div class="note dnote">D</div>
+<div class="note dsharpnote">D#</div>
+<div class="note enote">E</div>
+<div class="note fnote">F</div>
+<div class="note fsharpnote">F#</div>
+<div class="note gnote">G</div>
+<div class="note gsharpnote">G#</div>
+<div class="note anote">A</div>
+<div class="note asharpnote">A#</div>
+<div class="note bnote">B</div>
+</div>
+<div class="notes">
+<div class="note">Fifths</div>
+<div class="note bnote">B</div>
+<div class="note enote">E</div>
+<div class="note anote">A</div>
+<div class="note dnote">D</div>
+<div class="note gnote">G</div>
+<div class="note cnote">C</div>
+<div class="note fnote">F</div>
+<div class="note bflatnote">Bb</div>
+<div class="note eflatnote">Eb</div>
+<div class="note aflatnote">Ab</div>
+<div class="note dflatnote">Db</div>
+<div class="note gflatnote">Gb</div>
+</div>
+
+</body>
+</html>
