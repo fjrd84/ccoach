9e26f3aba94074147bc7a66df61558f6b71d355c
F. Javier R. Donado <fjrd84@gmail.com>
2015-01-02 03:02:52 +0100

    Added 7-segments font. JS animations in home.

---
+++ zf2/public/js/general.js
@@ -0,0 +1,26 @@
+$('.homeLogout').mouseenter(function () {
+    'use strict';
+    $('#navText').fadeIn('300');
+    $('#navText').text('Logout!');
+}).mouseleave(function () {
+    'use strict';
+    $('#navText').fadeOut('50');
+});
+
+$('.homePlay').mouseenter(function () {
+    'use strict';
+    $('#navText').fadeIn('300');
+    $('#navText').text('Play!');
+}).mouseleave(function () {
+    'use strict';
+    $('#navText').fadeOut('50');
+});
+
+$('.homeTraining').mouseenter(function () {
+    'use strict';
+    $('#navText').fadeIn('300');
+    $('#navText').text('Training!');
+}).mouseleave(function () {
+    'use strict';
+    $('#navText').fadeOut('50');
+});
