ef01dfda8b865363e6a04042c1ca2a54264c20c1
F. Javier R. Donado <fjrd84@gmail.com>
2015-01-07 02:54:22 +0100

    Training div added to home.

--- zf2/public/js/general.js
+++ zf2/public/js/general.js
@@ -3,18 +3,27 @@ $('.homeLogout').mouseenter(function () {
     $('#navText').fadeIn('300');
     $('#navText').text('Logout!');
 }).mouseleave(function () {
+        'use strict';
+        $('#navText').fadeOut('50');
+    });
+
+$('.showHome').mouseenter(function () {
     'use strict';
-    $('#navText').fadeOut('50');
-});
+    $('#navText').fadeIn('300');
+    $('#navText').text('Home!');
+}).mouseleave(function () {
+        'use strict';
+        $('#navText').fadeOut('50');
+    });
 
 $('.homePlay').mouseenter(function () {
     'use strict';
     $('#navText').fadeIn('300');
     $('#navText').text('Play!');
 }).mouseleave(function () {
-    'use strict';
-    $('#navText').fadeOut('50');
-});
+        'use strict';
+        $('#navText').fadeOut('50');
+    });
 
 $('.homeTraining').mouseenter(function () {
     'use strict';
@@ -24,3 +33,15 @@ $('.homeTraining').mouseenter(function () {
     'use strict';
     $('#navText').fadeOut('50');
 });
+
+function showTraining() {
+    'use strict';
+    $('.home').fadeOut(300,function(){$('.trainingView').fadeIn(600);});
+    //$('#homeContainer').flip();
+}
+
+function showHome() {
+    'use strict';
+    $('.trainingView').fadeOut(300,function(){$('.home').fadeIn(600);});
+    //$('#homeContainer').flip();
+}
