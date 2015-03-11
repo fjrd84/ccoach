6aa76b83d49d6d6cb4327304367b702d1e2badb9
F. Javier R. Donado <fjrd84@gmail.com>
2015-03-11 22:04:46 +0100

    CSS info simplify step 1.

--- zf2/public/js/game.js
+++ zf2/public/js/game.js
@@ -679,11 +679,23 @@ function sendScale() {
     sendData($('#comboBoxWrapper .comboBox').val());
 }
 
+function adjustFont(){
+    'use strict';
+    var newSize = $(window).width()/40;
+    newSize = Math.floor(newSize);
+    $('body').css('font-size', newSize+'px');
+}
+
 //////////////////////////////////////////////////////////////////////////////////////
 
+$(window).resize(function(){
+    adjustFont();
+});
 
 // Timer events
 setInterval(function () {
     'use strict';
     timerDown();
 }, 500);
+
+adjustFont();
